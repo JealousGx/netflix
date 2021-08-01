afb2fd0cc880b0e3016885f82ff6f3629e77d8cc
JealousGx <59867217+JealousGx@users.noreply.github.com>
2021-08-01 23:27:20 +0500

    Uploaded half server-side code

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./Routes/auth");
+const userRoute = require("./Routes/Users");
+const movieRoute = require("./Routes/Movies");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("DB connection successful!"))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+
+app.use("/api/auth/", authRoute);
+app.use("/api/users/", userRoute);
+app.use("/api/movies/", movieRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
