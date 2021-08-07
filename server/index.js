9ddcca74886367d1bb608f592cac612a39fc6063
JealousGx <59867217+JealousGx@users.noreply.github.com>
2021-08-07 14:05:23 +0500

    Added proxy for cors

--- server/index.js
+++ server/index.js
@@ -19,6 +19,10 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.use("/api/auth/", authRoute);
 app.use("/api/users/", userRoute);
